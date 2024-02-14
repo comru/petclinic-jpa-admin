e72e09f0c27c6a6c82e2f3f56a8cb244c1f315e1
alzlatoverov <alzlatoverov@gmail.com>
2024-02-14 18:25:52 +0400

    Fix PetFilter

--- src/main/java/io/amplicode/pja/api/dto/PetFilter.java
+++ src/main/java/io/amplicode/pja/api/dto/PetFilter.java
@@ -1,13 +1,10 @@
 package io.amplicode.pja.api.dto;
 
 import io.amplicode.pja.model.Pet;
-import jakarta.persistence.criteria.*;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.util.StringUtils;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 public record PetFilter(
         String q,
@@ -17,34 +14,6 @@ public record PetFilter(
         LocalDate birthDateGreaterThan,
         LocalDate birthDateLessThan
 ) {
-
-    public Specification<Pet> toSpecificationImprove() {
-        return (root, query, cb) -> {
-            List<Predicate> predicates = new ArrayList<>();
-            if (StringUtils.hasText(q)) {
-                predicates.add(cb.or(
-                        cb.like(cb.lower(root.get("name")), "%" + q.toLowerCase() + "%"),
-                        cb.like(cb.lower(root.get("type").get("name")), "%" + q.toLowerCase() + "%")
-                ));
-            }
-            if (ownerId != null) {
-                predicates.add(cb.equal(root.get("owner").get("id"), ownerId));
-            }
-            if (StringUtils.hasText(ownerFirstName)) {
-                predicates.add(cb.like(cb.lower(root.get("owner").get("firstName")), "%" + ownerFirstName.toLowerCase() + "%"));
-            }
-            if (StringUtils.hasText(ownerLastName)) {
-                predicates.add(cb.like(cb.lower(root.get("owner").get("lastName")), "%" + ownerLastName.toLowerCase() + "%"));;
-            }
-            if (birthDateLessThan != null) {
-                predicates.add(cb.lessThanOrEqualTo(root.get("birthDate"), birthDateLessThan));
-            }
-            if (birthDateGreaterThan != null) {
-                predicates.add(cb.greaterThanOrEqualTo(root.get("birthDate"), birthDateGreaterThan));
-            }
-            return cb.and(predicates.toArray(new Predicate[0]));
-        };
-    }
 
     public Specification<Pet> toSpecification() {
         return Specification.where(nameSpec().or(typeNameSpec()))
@@ -63,13 +32,13 @@ public record PetFilter(
 
     private Specification<Pet> typeNameSpec() {
         return ((root, query, cb) -> StringUtils.hasText(q)
-                ? cb.like(cb.lower(root.join("type").get("name")), "%" + q.toLowerCase() + "%")
+                ? cb.like(cb.lower(root.get("type").get("name")), "%" + q.toLowerCase() + "%")
                 : null);
     }
 
     private Specification<Pet> ownerIdSpec() {
         return ((root, query, cb) -> ownerId != null
-                ? cb.equal(root.join("owner", JoinType.LEFT).get("id"), ownerId)
+                ? cb.equal(root.get("owner").get("id"), ownerId)
                 : null);
     }
 
